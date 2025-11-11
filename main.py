b4dfc82e159535a1236adc492b5d34df5c14732c
buckmasterj7 <jlbuckmaster7@gmail.com>
2025-11-10 19:36:44 -0800

    add storage of user prompts

--- main.py
+++ main.py
@@ -1,18 +1,31 @@
 import os
 import sys
 from google import genai
+from google.genai import types
 from dotenv import load_dotenv
 
 def main():
     load_dotenv()
-    api_key = os.environ.get("GEMINI_API_KEY")
-    client = genai.Client(api_key=api_key)
+    
     if len(sys.argv) < 2:
         print("error: no prompt provided\nusage: uv run main.py 'your prompt here'")
         sys.exit(1)
+
+    api_key = os.environ.get("GEMINI_API_KEY")
+    client = genai.Client(api_key=api_key)
+
+    user_prompt = "".join(sys.argv[1:])
+
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=user_prompt)]),
+    ]
+
+    generate_content(client, messages)
+
+def generate_content(client, messages):
     response = client.models.generate_content(
         model = "gemini-2.0-flash-001",
-        contents = "".join(sys.argv[1:])
+        contents = messages,
     )
     print("Prompt tokens:", response.usage_metadata.prompt_token_count)
     print("Response tokens:", response.usage_metadata.candidates_token_count)
