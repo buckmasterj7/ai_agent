45ac64dad3eef82e6253c3be628a67ecf961f3af
buckmasterj7 <jlbuckmaster7@gmail.com>
2025-12-01 17:07:16 -0800

    added run python file function

--- functions/write_file.py
+++ functions/write_file.py
@@ -5,15 +5,16 @@ def write_file(working_directory, file_path, content):
     abs_working_dir = os.path.abspath(working_directory)
     target_file = os.path.abspath(os.path.join(working_directory, file_path))
 
-    if not target_file.startswith(abs_working_dir):
+    if not (target_file == abs_working_dir or target_file.startswith(abs_working_dir + os.sep)):
         return f'Error: Cannot write to "{file_path}" as it is outside the permitted working directory'
     
     try:
+        os.makedirs(os.path.dirname(target_file), exist_ok=True)
         with open(target_file, "w") as file:
             file.write(content)
-            return f'Successfully wrote to "{file_path}" ({len(content)} characters written)'
+        return f'Successfully wrote to "{file_path}" ({len(content)} characters written)'
     except Exception as e:
-        return f"Error writing files: {e}"
+        return f"Error: {e}"
 
 
 
--- tests.py
+++ tests.py
@@ -1,18 +1,33 @@
-from functions.write_file import write_file
+from functions.run_python_file import run_python_file
 
 def test():
-    result = write_file("calculator", "lorem.txt", "wait, this isn't lorem ipsum")
+    result = run_python_file("calculator", "main.py")
+    print("Result for main.py file:")
+    print(result)
+    print("")
+
+    result = run_python_file("calculator", "main.py", ["3 + 5"])
+    print("Result for main.py file:")
+    print(result)
+    print("")
+
+    result = run_python_file("calculator", "tests.py")
+    print("Result for tests.py file:")
+    print(result)
+    print("")
+
+    result = run_python_file("calculator", "../main.py")
+    print("Result for ../main.py file:")
+    print(result)
+    print("")
+
+    result = run_python_file("calculator", "nonexistent.py")
+    print("Result for nonexistent.py file:")
+    print(result)
+    print("")
+
+    result = run_python_file("calculator", "lorem.txt")
     print("Result for lorem.txt file:")
-    print(result)
-    print("")
-
-    result = write_file("calculator", "pkg/morelorem.txt", "lorem ipsum dolor sit amet")
-    print("Result for pkg/morelorem.txt file:")
-    print(result)
-    print("")
-
-    result = write_file("calculator", "/tmp/temp.txt", "this should not be allowed")
-    print("Result for /tmp/temp.txt file:")
     print(result)
     print("")
 
